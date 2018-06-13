0d0be5a52497b16669303cb5a18086233733c27a
Evelyn Galvez <evelyn.galvez.p@gmail.com>
2018-06-13 08:47:52 -0400

    ejercicio realizado en clases

---
+++ assets/js/main.js
@@ -0,0 +1,40 @@
+const boton = document.getElementById('btn');
+boton.addEventListener('click', () => {
+  //acá guardo el comentario ingresado por el usuario
+  let comments = document.getElementById('comment').value;
+
+  //limpiar el textarea
+  document.getElementById('comment').value = '';
+
+  //contenedor donde dejaré mis comentarios en html
+  const cont = document.getElementById('cont');
+
+  
+  //crear un div contenedor
+  const newComments = document.createElement('div')
+  //validar que el textarea tenga un comentario
+  if (comments.length == 0 || comments === null) {
+    alert('Debes ingresar un mensaje');
+    return false;
+  }
+  //crear checkbox
+  const chck = document.createElement('input');
+  chck.type = 'checkbox';
+  //crear icono corazón
+  const heart = document.createElement('i');
+  heart.classList.add('fas', 'fa-heart', 'heart');
+  //crear icono basura
+  const trash = document.createElement('i');
+  trash.classList.add('fas', 'fa-trash', 'trash');
+  //nodos de texto del textarea
+  let textNewComment = document.createTextNode(comments);
+
+  const contenedorElemento = document.createElement('p');
+  contenedorElemento.appendChild(textNewComment);
+  newComments.appendChild(chck);
+  newComments.appendChild(trash);
+  newComments.appendChild(heart);
+  newComments.appendChild(contenedorElemento);
+
+  cont.appendChild(newComments);
+})
